3b1fe8191e26bea9ebe93e4bf45fe5de67bb5cb1
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-06-23 13:43:14 +0300

    create new project, set upwork gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+"use strict";
+const { src, dest, watch, series } = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const minify = require("gulp-minify");
+const htmlmin = require("gulp-htmlmin");
+
+// Static server
+function bs() {
+  serveSass();
+  browserSync.init({
+    server: {
+      baseDir: "src/",
+    },
+  });
+  watch("src/*.html").on("change", browserSync.reload);
+  watch("src/sass/**/*.sass", serveSass);
+  watch("src/js/*.js").on("change", browserSync.reload);
+}
+
+function serveSass() {
+  return src("src/sass/*.sass")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(dest("src/style"))
+    .pipe(browserSync.stream());
+}
+
+function buildCSS(done) {
+  src("src/style/**/**.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(dest("dist/style"));
+  done();
+}
+
+function buildJS(done) {
+  src("src/js/**/**.js")
+    .pipe(
+      minify({
+        ext: {
+          min: ".js",
+        },
+      })
+    )
+    .pipe(dest("dist/js"));
+  done();
+}
+
+function buildHTML(done) {
+  src("src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("dist/"));
+  done();
+}
+
+function buildFonts(done) {
+  src("src/fonts/**").pipe(dest("dist/fonts"));
+  done();
+}
+
+exports.serve = bs;
+exports.build = series(buildCSS, buildJS, buildHTML, buildFonts);
